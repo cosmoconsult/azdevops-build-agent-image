0a68d2e287c02869ddaff78a03c00b9eb6476fde
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-04-25 15:50:00 +0200

    download agent during build to speed up container start

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,22 @@
 # escape=`
 ARG BASE
+ARG AZP_TOKEN
+ARG AZP_URL
 FROM mcr.microsoft.com/windows/servercore:$BASE
 
 WORKDIR /azp
 COPY start.ps1 .
 RUN powershell -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));" `
-    "choco install -y docker-cli;"
+    "choco install -y docker-cli;" 
+
+RUN powershell -Command "New-Item `"\azp\agent`" -ItemType directory | Out-Null;" `
+    "Set-Location agent;" `
+    "$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(`":$AZP_TOKEN`"));" `
+    "$package = Invoke-RestMethod -Headers @{Authorization=(`"Basic $base64AuthInfo`")} `"$AZP_URL/_apis/distributedtask/packages/agent?platform=win-x64&``$top=1`";" `
+    "$packageUrl = $package[0].Value.downloadUrl;" `
+    "Write-Host $packageUrl;" `
+    "$wc = New-Object System.Net.WebClient;" `
+    "$wc.DownloadFile($packageUrl, `"$(Get-Location)\agent.zip`");" `
+    "Expand-Archive -Path `"agent.zip`" -DestinationPath `"\azp\agent`""
+
 CMD powershell .\start.ps1
--- start.ps1
+++ start.ps1
@@ -19,31 +19,13 @@ if ($Env:AZP_WORK -and -not (Test-Path Env:AZP_WORK)) {
   New-Item $Env:AZP_WORK -ItemType directory | Out-Null
 }
 
-New-Item "\azp\agent" -ItemType directory | Out-Null
-
 # Let the agent ignore the token env variables
 $Env:VSO_AGENT_IGNORE = "AZP_TOKEN,AZP_TOKEN_FILE"
 
 Set-Location agent
-
-Write-Host "1. Determining matching Azure Pipelines agent..." -ForegroundColor Cyan
-
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$(Get-Content ${Env:AZP_TOKEN_FILE})"))
-$package = Invoke-RestMethod -Headers @{Authorization=("Basic $base64AuthInfo")} "$(${Env:AZP_URL})/_apis/distributedtask/packages/agent?platform=win-x64&`$top=1"
-$packageUrl = $package[0].Value.downloadUrl
-
-Write-Host $packageUrl
-
-Write-Host "2. Downloading and installing Azure Pipelines agent..." -ForegroundColor Cyan
-
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($packageUrl, "$(Get-Location)\agent.zip")
-
-Expand-Archive -Path "agent.zip" -DestinationPath "\azp\agent"
-
 try
 {
-  Write-Host "3. Configuring Azure Pipelines agent..." -ForegroundColor Cyan
+  Write-Host "Configuring Azure Pipelines agent..." -ForegroundColor Cyan
 
   .\config.cmd --unattended `
     --agent "$(if (Test-Path Env:AZP_AGENT_NAME) { ${Env:AZP_AGENT_NAME} } else { ${Env:computername} })" `
@@ -57,7 +39,7 @@ try
   # remove the administrative token before accepting work
   Remove-Item $Env:AZP_TOKEN_FILE
 
-  Write-Host "4. Running Azure Pipelines agent..." -ForegroundColor Cyan
+  Write-Host "Running Azure Pipelines agent..." -ForegroundColor Cyan
 
   .\run.cmd --once
 }
